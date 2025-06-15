3e1c9030492b8e24c697d2da53216a735e286c1a
RodrigoCorrea31 <rodrigocorreamuse@gmail.com>
2025-06-15 12:35:42 -0300

    Agregué la clase MyDbContextFactory en la capa de Persistencia para permitir la migración a SQL Server, ya que el uso de User Secrets generaba un error al intentar crear el contexto en tiempo de diseño.

---
+++ E-CommerceSalesCars.Persistencia/Data/MyDbContextFactory.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+using System.Reflection;
+
+namespace E_CommerceSalesCars.Persistencia.Data
+{
+    public class MyDbContextFactory : IDesignTimeDbContextFactory<MyDbContext>
+    {
+        public MyDbContext CreateDbContext(string[] args)
+        {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true)
+                .AddUserSecrets(typeof(MyDbContextFactory).Assembly)
+                .Build();
+
+            var optionsBuilder = new DbContextOptionsBuilder<MyDbContext>();
+            var connectionString = config.GetConnectionString("StrConn");
+
+            optionsBuilder.UseSqlServer(connectionString);
+
+            return new MyDbContext(optionsBuilder.Options);
+        }
+    }
+}
--- E-CommerceSalesCars/Program.cs
+++ E-CommerceSalesCars/Program.cs
@@ -18,7 +18,7 @@ namespace E_CommerceSalesCars
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
             builder.Services.AddDbContext<MyDbContext>(options =>
-                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+                options.UseSqlServer(builder.Configuration.GetConnectionString("StrConn")));
 
 
 
