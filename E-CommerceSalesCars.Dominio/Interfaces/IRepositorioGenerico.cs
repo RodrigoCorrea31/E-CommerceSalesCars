4e38125553781ab723103e8137ff633f867ef01f
RodrigoCorrea31 <rodrigocorreamuse@gmail.com>
2025-07-18 09:38:21 -0300

    Implementación de un repositorio genérico para evitar la duplicación de código en operaciones CRUD básicas, facilitando la reutilización y mantenibilidad en todos los repositorios.

---
+++ E-CommerceSalesCars.Dominio/Interfaces/IRepositorioGenerico.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_CommerceSalesCars.Dominio.Interfaces
+{
+    public interface IRepositorioGenerico<E> where E : class
+    {
+        Task<E> ObtenerPorIdAsync(int id);
+        Task<IEnumerable<E>> ListarTodosAsync();
+        Task AgregarAsync(E Entidad);
+        void ModificarAsync(E Entidad);
+        void EliminarAsync(E Entidad);
+    }
+}
---
+++ E-CommerceSalesCars.Persistencia/Repositorios/RepositorioGenerico.cs
@@ -0,0 +1,48 @@
+﻿using E_CommerceSalesCars.Dominio.Interfaces;
+using E_CommerceSalesCars.Persistencia.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_CommerceSalesCars.Persistencia.Repositorios
+{
+    public class RepositorioGenerico<E> : IRepositorioGenerico<E> where E : class
+    {
+        private readonly MyDbContext _context;
+        private readonly DbSet<E> _dbset;
+
+        public RepositorioGenerico(MyDbContext context)
+        {
+            _context = context;
+            _dbset = _context.Set<E>();
+        }
+
+        public async Task<E> ObtenerPorIdAsync(int id)
+        {
+            return await _dbset.FindAsync(id);
+        }
+
+        public async Task<IEnumerable<E>> ListarTodosAsync()
+        {
+            return await _dbset.ToListAsync();
+        }
+
+        public async Task AgregarAsync(E entidad)
+        {
+            await _dbset.AddAsync(entidad);
+        }
+
+        public void EliminarAsync(E entidad)
+        {
+            _dbset.Remove(entidad);
+        }
+
+        public void ModificarAsync(E entidad)
+        {
+            _dbset.Update(entidad);
+        }
+    }
+}
