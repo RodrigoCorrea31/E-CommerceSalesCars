385bae68c903422c1ac48f72f7dfc137e9d3c262
RodrigoCorrea31 <rodrigocorreamuse@gmail.com>
2025-07-24 15:34:37 -0300

    Se implementaron las interfaces de los repositorios específicos para Oferta, Transacción y Usuario.

---
+++ E-CommerceSalesCars.Dominio/Interfaces/IRepositorioOferta.cs
@@ -0,0 +1,16 @@
+﻿using E_CommerceSalesCars.Dominio.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_CommerceSalesCars.Dominio.Interfaces
+{
+    public interface IRepositorioOferta
+    {
+        Task<ICollection<Oferta>> ObtenerOfertasPorPublicacionAsync(int publicacionId);
+        Task AceptarOfertaAsync(int ofertaId);
+        Task RechazarOfertaAsync(int ofertaId);
+    }
+}
---
+++ E-CommerceSalesCars.Dominio/Interfaces/IRepositorioUsuario.cs
@@ -0,0 +1,18 @@
+﻿using E_CommerceSalesCars.Dominio.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_CommerceSalesCars.Dominio.Interfaces
+{
+    public interface IRepositorioUsuario
+    {
+        Task<Usuario> ObtenerPorEmailAsync(string email);
+        Task<bool> ExisteUsuarioConEmailAsync(string email);
+        Task<ICollection<Oferta>> ObtenerOfertasRealizadasAsync(int usuarioId);
+        Task<ICollection<Transaccion>> ObtenerComprasAsync(int usuarioId);
+        Task<ICollection<Transaccion>> ObtenerVentasAsync(int usuarioId);
+    }
+}
