3925f2e809d6de7a1fd6313d95fde2c52e897eed
RodrigoCorrea31 <rodrigocorreamuse@gmail.com>
2025-07-29 11:19:20 -0300

    Implementada la interfaz de Servicio Publicacion.

---
+++ E-CommerceSalesCars.Dominio/Interfaces/IServicioPublicacion.cs
@@ -0,0 +1,18 @@
+﻿using E_CommerceSalesCars.Dominio.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_CommerceSalesCars.Dominio.Interfaces
+{
+    public interface IServicioPublicacion
+    {
+        Task CrearPublicacionAsync(Publicacion publicacion);
+        Task EditarPublicacionAsync(int id, Publicacion publicacion);
+        Task EliminarPublicacionAsync(int id);
+        Task<List<Publicacion>> ObtenerPublicacionesAsync();
+        Task<List<Publicacion>> FiltrarPublicacionesAsync(Publicacion publicacionFiltro);
+    }
+}
