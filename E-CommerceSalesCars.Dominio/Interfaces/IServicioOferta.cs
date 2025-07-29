6f3fbd1438ce597258d2e477e8c8f5dba1196dde
RodrigoCorrea31 <rodrigocorreamuse@gmail.com>
2025-07-29 11:26:32 -0300

    Implementada la interfaz de Servicio Oferta

---
+++ E-CommerceSalesCars.Dominio/Interfaces/IServicioOferta.cs
@@ -0,0 +1,16 @@
+﻿using E_CommerceSalesCars.Dominio.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_CommerceSalesCars.Dominio.Interfaces
+{
+    public interface IServicioOferta
+    {
+        Task RealizarOfertaAsync(Oferta oferta);
+        Task AceptarOfertaAsync(int ofertaId);
+        Task RechazarOfertaAsync(int ofertaId);
+    }
+}
